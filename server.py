3355c9681f4f39ce9c8bc21029cefde97edaa701
Richard <richard@nanowire.nat.physics.ox.ac.uk>
2018-02-27 12:44:24 +0000

    surely now

--- server.py
+++ server.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
             print "connection received from: ", addr
             while True:
                 data = connection.recv(1024)
+
                 if data[0:8] == "filename":
                     sputterer_filename += data [10:]
                     print "sputterer filename = " + sputterer_filename
                     continue
-                if data == "3D printing isn't even that cool":
-                    break
                 sputterer_data += data
                 print "sputterer form data: \n\n", sputterer_data
+                break
 
             with open("data/" + sputterer_filename, 'w') as in_file:
                 in_file.write(sputterer_data)
